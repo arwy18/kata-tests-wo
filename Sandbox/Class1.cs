55812ec2133d2d696734066d2f58e74557993a55
Willy Osorto <arwy1806@gmail.com>
2020-06-26 13:38:54 -0600

    Initial commit

---
+++ Sandbox/Class1.cs
@@ -0,0 +1,8 @@
+﻿namespace Sandbox
+{
+    public class Calculator
+    {
+        public static int Add(int x, int y) => x + y;
+        public static int Subtract(int x, int y) => x - y;
+    }
+}
---
+++ Tests/Tests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void PassingTest()
+        {
+            Assert.AreEqual(4, Calculator.Add(2,2));
+        }
+
+        [Test]
+        public void FailingTest()
+        {
+            Assert.AreEqual(5, Calculator.Add(2,2));
+        }
+        
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+
+    public class Calculator
+    {
+        public static int Add(int x, int y) => x + y;
+        public static int Subtract(int x, int y) => x - y;
+    }
+}
